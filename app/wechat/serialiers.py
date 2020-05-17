5d27aeee0c453d1acb8037a685a2c73231f9abe2
Tc <jackgoodchen2019@gmail.com>
2020-05-17 11:36:17 +0800

    init

--- app/wechat/serialiers.py
+++ app/wechat/serialiers.py
@@ -15,6 +15,14 @@ class AccQrcodeModelSerializer(serializers.ModelSerializer):
 
     tags = serializers.SerializerMethodField()
     lists = serializers.SerializerMethodField()
+    acc = serializers.SerializerMethodField()
+    createtime = serializers.SerializerMethodField()
+
+    def get_createtime(self,obj):
+        return UtilTime().timestamp_to_string(obj.createtime,format_v="%Y-%m-%d")
+
+    def get_acc(self,obj):
+        return AccSerializer(AccQrcode.objects.get(id=obj.accid),many=False).data
 
     def get_tags(self,obj):
         return AccTagModelSerializer(AccTag.objects.filter(id__in=json.loads(obj.tags)).order_by('-createtime'),many=True).data
@@ -25,7 +33,7 @@ class AccQrcodeModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AccQrcode
-        fields = ('id','name','accid','tot_count','new_count','follow_count','type','endtime','qr_type','send_type','url','tags','lists',)
+        fields = ('id','name','accid','tot_count','createtime','acc','new_count','follow_count','type','endtime','qr_type','send_type','url','tags','lists',)
 
 class AccQrcodeListModelSerializer(serializers.ModelSerializer):
 
