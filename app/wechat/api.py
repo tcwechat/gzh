1fc506d931cfaba581412a9b779114f30070198e
Tc <jackgoodchen2019@gmail.com>
2020-05-24 09:16:08 +0800

    init

--- app/wechat/api.py
+++ app/wechat/api.py
@@ -322,6 +322,7 @@ class WeChatAPIView(viewsets.ViewSet):
             tags = json.loads(item.tags)
             if int(tagid) not in tags:
                 tags.append(tagid)
+                item.tags=json.dumps(item.tags)
                 item.save()
             openids.append(item.openid)
 
@@ -350,6 +351,7 @@ class WeChatAPIView(viewsets.ViewSet):
             tags = json.loads(item.tags)
             if int(tagid) not in tags:
                 tags.append(tagid)
+                item.tags=json.dumps(item.tags)
                 item.save()
             openids.append(item.openid)
 
