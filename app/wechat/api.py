e311bf227894e9c0bd8b0e4572e585ad90f38467
Tc <jackgoodchen2019@gmail.com>
2020-06-20 23:41:00 +0800

    init

--- app/wechat/api.py
+++ app/wechat/api.py
@@ -1869,15 +1869,16 @@ class WeChatAPIView(viewsets.ViewSet):
             c+=1
         tables.reverse()
 
-        j = sum([ item1[0].num for item in tables for item1 in item])
-        i=1
-        while i<7:
+        j = sum([item[0]['num'] for item in tables])
+
+        i = 1
+        while i < 7:
             tot = 0
             for item in tables:
-                for item1 in item:
-                    tot += item1[i].num if len(item1)-1>=i else 0
-            i+=1
-            agree.append(round(tot * 100.0 / j if j else 0.0,2))
+                tot += item[i]['num'] if len(item) - 1 >= i else 0
+
+            i += 1
+            agree.append(round(tot * 100.0 / j if j else 0.0, 2))
 
         return {"data":{
             "tables":tables,
--- test/1.py
+++ test/1.py
@@ -1,26 +1,204 @@
 
-import json
-
-def splitTag(st):
-
-    d=[]
-    o = st
-    while o and len(o):
-        o = o.split("<")
-        d.append(o[0])
-        if len(o)>1:
-            o=o[1].split("/>")
-            print(o)
-            d.append(o[0])
-            o=o[1]
-            print(o)
-        else:
-            break
-
-    print(d)
 
 
+tables=[
+        [
+            {
+                "date": "2020-06-17",
+                "num": 1
+            },
+            {
+                "date": "2020-06-18",
+                "num": 1,
+                "rate": 100.0
+            }
+        ],
+        [
+            {
+                "date": "2020-06-16",
+                "num": 0
+            },
+            {
+                "date": "2020-06-17",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-18",
+                "num": 0,
+                "rate": 0.0
+            }
+        ],
+        [
+            {
+                "date": "2020-06-15",
+                "num": 0
+            },
+            {
+                "date": "2020-06-16",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-17",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-18",
+                "num": 0,
+                "rate": 0.0
+            }
+        ],
+        [
+            {
+                "date": "2020-06-14",
+                "num": 0
+            },
+            {
+                "date": "2020-06-15",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-16",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-17",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-18",
+                "num": 0,
+                "rate": 0.0
+            }
+        ],
+        [
+            {
+                "date": "2020-06-13",
+                "num": 0
+            },
+            {
+                "date": "2020-06-14",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-15",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-16",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-17",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-18",
+                "num": 0,
+                "rate": 0.0
+            }
+        ],
+        [
+            {
+                "date": "2020-06-12",
+                "num": 0
+            },
+            {
+                "date": "2020-06-13",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-14",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-15",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-16",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-17",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-18",
+                "num": 0,
+                "rate": 0.0
+            }
+        ],
+        [
+            {
+                "date": "2020-06-11",
+                "num": 0
+            },
+            {
+                "date": "2020-06-12",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-13",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-14",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-15",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-16",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-17",
+                "num": 0,
+                "rate": 0.0
+            },
+            {
+                "date": "2020-06-18",
+                "num": 0,
+                "rate": 0.0
+            }
+        ]
+    ]
 
-a="""123<{"title":"粉丝昵称"}/>ffdsfdsf<{"title":"粉丝昵称"}/>"""
+# for item in tables:
+#     print(item[0]['num'])
+agree=[]
+j = sum([item[0]['num'] for item in tables])
 
-splitTag(a)
+i = 1
+while i < 7:
+    tot = 0
+    for item in tables:
+        tot += item[i]['num'] if len(item) - 1 >= i else 0
+
+    i += 1
+    agree.append(round(tot * 100.0 / j if j else 0.0, 2))
+
+
+print(agree)
